82703de2c7f0e36d57f25775d17d5eb4cc07d060
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2022-10-12 16:44:55 +0700

    update Readme

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
+[![Build Status](https://app.travis-ci.com/bhokin/demo-pyci.svg?branch=main)](https://app.travis-ci.com/github/bhokin/demo-pyci)
+[![codecov](https://codecov.io/gh/bhokin/demo-pyci/branch/main/graph/badge.svg?token=C1JRNC4XM9)](https://codecov.io/gh/bhokin/demo-pyci)
 Continuous Integration Demo
 ============================
-[![Build Status](https://travis-ci.com/jbrucker/demo-pyci.svg?branch=master)](https://travis-ci.com/jbrucker/demo-pyci)
 
-This project demonstrates use of Github Actions to build and test a Python project.  
-Create a repository on Github, then use Github Actions to build and test it.
+This project demonstrates use of Travis CI to build and test a Python project.  
+You will create a repository on Github, then have Travis-CI pull and test it,
+according to a "script" of instructions.
 
-Next: you will add an online code coverage report for your tests using <https://codecov.io>
+After that, you will add an online code coverage report for your tests.
 
 ## Instructions
 
-These are somewhat out of date, since they apply to Travis-CI:
-<https://cpske.github.io/ISP/automation/travis-demo-project>
-
-
+Please see: https://cpske.github.io/ISP/automation/travis-demo-project
--- test_statistics.py
+++ test_statistics.py
@@ -1,16 +1,14 @@
 from unittest import TestCase
-from statistics import variance,stdev
-
-
+from statistics import variance, stdev
 
 
 class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
@@ -19,11 +17,16 @@ class StatisticsTest(TestCase):
 
     def test_std_dev(self):
         "std is zero if all same"
-        data= [10.0,10.0,10.0]
-        self.assertEqual(0.0,stdev(data))
-        self.assertEqual(2.0,variance([1,2,3,4,5]))
+        data = [10.0, 10.0, 10.0]
+        self.assertEqual(0.0, stdev(data))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+
+    def test_variance_single_value_is_zero(self):
+        """variance of a singleton list should be zero"""
+        self.assertEqual(0.0, variance([0.0]))
+        self.assertEqual(0.0, variance([99.999]))
+
 
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
